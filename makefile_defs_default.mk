502fa35dda5132bcc143a1458c259ac6f2239042
Brent Barker <me@brentwbarker.net>
2016-08-10 23:35:41 -0400

    Initial commit

---
+++ makefile_defs_default.mk
@@ -0,0 +1,18 @@
+MADAI_HOME = /home/bbarker/proj/source-imaging/coral/
+#prefix of .../rhic directory (root of madai installation)
+MADAI_GSLPATH = /usr/local
+#where gnu scientific library is installed
+MADAI_X11PATH = /usr/X11R6
+#root of X11 installation
+MADAI_HDF5_HOME = /opt/local
+#root of HDF5 installation
+#MADAI_CPP = /usr/bin/clang++
+MADAI_CPP = g++
+#MADAI_CPP = /opt/local/bin/g++-mp-4.6
+#compiler
+MADAI_INSTALLDIR = /home/bbarker/proj/source-imaging/coral/local
+#location of where you want things installed
+#MADAI_CFLAGS = -O
+#MADAI_CFLAGS = -fast
+MADAI_CFLAGS = -O2 -ggdb
+#compiler optimization flags, usually -O2 for linux, -fast for OSX with g++
